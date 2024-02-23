12849694621e1df0a534a461e0df397d29d1e663
Helen Cris <helencristavares@gmail.com>
2024-02-23 20:31:50 -0300

    🎉 init: initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, Text } from 'react-native';
+
+
+export default function App() {
+
+	return (
+		<>
+			<StatusBar style="light" />
+
+			<SafeAreaView><Text>Projeto Extensionista</Text></SafeAreaView>
+		</>
+	);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto Extensionista App
+
